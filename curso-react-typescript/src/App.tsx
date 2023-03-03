b0d07acc972e499272c1e3d55a6d25504c3ca048
Pablo Ramon Varela de Souza <pabloramon044@gmail.com>
2023-03-03 10:52:11 -0300

    Adição do react router dom e rota de teste

--- curso-react-typescript/src/App.tsx
+++ curso-react-typescript/src/App.tsx
@@ -1,7 +1,10 @@
+import { BrowserRouter } from "react-router-dom";
+import { AppRoutes } from "./routes";
+
 export const App = () => {
   return (
-    <div className="App"> 
-      TESTE TESTADO     
-    </div>
+    <BrowserRouter>
+      <AppRoutes/>
+    </BrowserRouter>
   );
 }
