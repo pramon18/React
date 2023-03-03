c2a537a5362f5c59ddb10a2ac5748c46c562a584
Pablo Ramon Varela de Souza <pabloramon044@gmail.com>
2023-03-03 16:58:06 -0300

    Início menu lateral

---
+++ curso-react-typescript/src/shared/components/menu-lateral/MenuLateral.tsx
@@ -0,0 +1,35 @@
+import { Avatar, Divider, Drawer, Icon, List, ListItemButton, ListItemIcon, ListItemText, useTheme } from '@mui/material';
+import {Box} from '@mui/system';
+import { Home } from '@mui/icons-material';
+
+export const MenuLateral: React.FC<{children: React.ReactNode}> = ({children}) => {
+  const theme = useTheme();
+
+  return(
+    <>
+      <Drawer variant='permanent'>
+        <Box width={theme.spacing(28)} height="100%" display="flex" flexDirection='column'>
+          <Box width="100%" height={theme.spacing(20)} display="flex" alignItems="center" justifyContent="center">
+            <Avatar 
+              sx={{height: theme.spacing(12), width : theme.spacing(12)}} 
+              src='logo192.png'/>
+          </Box>          
+          <Divider />
+          <Box flex={1}>
+            <List component='nav'>
+              <ListItemButton>
+                <ListItemIcon>
+                  <Home></Home>
+                </ListItemIcon>
+                <ListItemText primary="Inbox" />
+              </ListItemButton>
+            </List>
+          </Box>
+        </Box>
+      </Drawer>
+      <Box height="100vh" marginLeft={theme.spacing(28)}>
+        { children }
+      </Box>            
+    </>    
+  );
+};
