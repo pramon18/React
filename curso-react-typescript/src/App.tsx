13aabc864d18144de9e45dc975e7d5969aecd9c6
Pablo Ramon Varela de Souza <pabloramon044@gmail.com>
2023-03-03 12:06:00 -0300

    Adicionado troca de tema

--- curso-react-typescript/src/App.tsx
+++ curso-react-typescript/src/App.tsx
@@ -2,14 +2,15 @@ import { ThemeProvider } from "@mui/material";
 import { BrowserRouter } from "react-router-dom";
 
 import { AppRoutes } from "./routes";
+import { AppThemeProvider } from "./shared/contexts/ThemeContext";
 import { LightTheme } from "./shared/themes";
 
 export const App = () => {
   return (
-    <ThemeProvider theme={LightTheme}>
+    <AppThemeProvider>
       <BrowserRouter>
         <AppRoutes />
       </BrowserRouter>
-    </ThemeProvider>
+    </AppThemeProvider>
   );
 }
--- curso-react-typescript/src/routes/index.tsx
+++ curso-react-typescript/src/routes/index.tsx
@@ -1,10 +1,13 @@
 import Button from "@mui/material/Button";
 import { Routes, Route, Navigate } from "react-router-dom";
+import { useAppThemeContext } from "../shared/contexts";
 
 export const AppRoutes = () => {
+    const {toggleTheme} = useAppThemeContext();
+
     return(
         <Routes>
-            <Route path="/pagina-inicial" element={<Button variant="contained" color="primary"> <p>Página Inicial</p></Button>}/>
+            <Route path="/pagina-inicial" element={<Button variant="contained" color="primary" onClick={toggleTheme}> <p>TOGGLE THEME</p></Button>}/>
             <Route path="*" element ={<Navigate to="/pagina-inicial"/>}/>
         </Routes>
     );
