5cd90df8dc240dad1edae0758421e16553494a57
Pablo Ramon Varela de Souza <pabloramon044@gmail.com>
2023-03-03 11:24:51 -0300

    Adição de tema claro

--- curso-react-typescript/src/routes/index.tsx
+++ curso-react-typescript/src/routes/index.tsx
@@ -4,7 +4,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 export const AppRoutes = () => {
     return(
         <Routes>
-            <Route path="/pagina-inicial" element={<Button> <p>Página Inicial</p></Button>}/>
+            <Route path="/pagina-inicial" element={<Button variant="contained" color="primary"> <p>Página Inicial</p></Button>}/>
             <Route path="*" element ={<Navigate to="/pagina-inicial"/>}/>
         </Routes>
     );
