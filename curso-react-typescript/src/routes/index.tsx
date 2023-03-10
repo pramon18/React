945d2006d42a50408513524c2803e655c8f14214
Pablo Ramon Varela de Souza <pabloramon044@gmail.com>
2023-03-10 14:54:47 -0300

    Adicionada responsividade no menu lateral

--- curso-react-typescript/src/routes/index.tsx
+++ curso-react-typescript/src/routes/index.tsx
@@ -1,14 +1,16 @@
+import { Box } from '@mui/material';
 import Button from '@mui/material/Button';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
-import { useAppThemeContext } from '../shared/contexts';
+import { useAppThemeContext, useDrawerContext } from '../shared/contexts';
 
 export const AppRoutes = () => {
+  const { toggleDrawerOpen } = useDrawerContext();
   const { toggleTheme } = useAppThemeContext();
 
   return(
     <Routes>
-      <Route path="/pagina-inicial" element={<Button variant="contained" color="primary" onClick={toggleTheme}> <p>TOGGLE THEME</p></Button>}/>
+      <Route path="/pagina-inicial" element={<Box><Button variant="contained" color="primary" onClick={toggleDrawerOpen}> <p>TOGGLE DRAWER</p></Button> <Button variant="contained" color="primary" onClick={toggleTheme}> <p>TOGGLE THEME</p></Button></Box>}/>      
 
       <Route path="*" element ={<Navigate to="/pagina-inicial"/>}/>
     </Routes>
